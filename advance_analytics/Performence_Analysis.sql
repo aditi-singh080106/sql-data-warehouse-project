8908029ed9582ea3c70304d79d756320d8d488d7
Aditi Singh <aditi08092003@gmail.com>
2025-09-13 00:55:01 +0530

    Update Performence_Analysis.sql

--- advance_analytics/Performence_Analysis.sql
+++ advance_analytics/Performence_Analysis.sql
@@ -16,9 +16,22 @@ order_year ,
 product_name 
 ORDER BY product_name) 
 SELECT 
-* ,
+    order_year,
+    product_name,
+    current_sales,
 AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales ,
-current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg_sales 
+current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg_sales ,
+CASE WHEN (current_sales - AVG(current_sales) OVER (PARTITION BY product_name)) > 0 THEN 'Above Average' 
+     WHEN (current_sales - AVG(current_sales) OVER (PARTITION BY product_name)) < 0 THEN 'Below Average' 
+     ELSE 'Average' 
+     END AS performace ,
+-- -- YEAR OVER YEAR ANALYSIS 
+LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS previous_year_sales , 
+current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS previous_year_diff ,
+CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increse' 
+	 WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease' 
+     ELSE 'No Change' 
+     END AS previous_year_change
 FROM yearly_product_sales  ;
 
 
